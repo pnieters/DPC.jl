1d67d5a7078f4c8ed6a07af7b87245b7bc907901
jleugeri <johannes@leugering.science>
2021-03-02 02:15:03 +0100

    latch example

--- examples/latch.jl
+++ examples/latch.jl
@@ -30,12 +30,14 @@ input=sort!([
 
 logger=simulate!(net, input, 1000)
 n    = filter(x->(x.object==:n_storage), logger.data)
+n_storage_spikes = filter(x->(x.object==:n_storage && x.event == :spikes), logger.data)
 syn1   = filter(x->(x.object==:syn1), logger.data)
 syn2   = filter(x->(x.object==:syn2), logger.data)
 
 steps!(ax1, [0;syn1.t;1000], 1 .+ 0.45 .* [0;Int.(syn1.state);0] , fill=RGBAf0(1,0,0,0.5))
 steps!(ax1, [0;syn2.t;1000], 2 .+ 0.45 .* [0;Int.(syn2.state);0] , fill=RGBAf0(0,0,1,0.5))
-steps!(ax1, [0;n.t;1000], 0.45 .* [0;Int.(n.state);0] , fill=RGBAf0(0,1,0,0.5))
+# steps!(ax1, [0;n.t;1000], 0.45 .* [0;Int.(n.state);0] , fill=RGBAf0(0,1,0,0.5))
+linesegments!(ax1, repeat(n_storage_spikes.t,inner=2), repeat([0,0.9], outer=length(n_storage_spikes.t)))
 
 ################################################################################
 
@@ -64,22 +66,24 @@ synapses:
 input=sort!([
     [Event(:input_spikes, 0.0,  t, objects[:i_set]) for t ∈ [105.0, 1105.0]];
     [Event(:input_spikes, 0.0,  t, objects[:i_reset]) for t ∈ [400.0, 1400.0]];
-    [Event(:input_spikes, 0.0,  t, objects[:i_read]) for t ∈ [150.0, 300.0, 450.0, 600.0, 750.0, 900.0, 1050.0, 1300.0, 1450.0, 1600.0]];
+    [Event(:input_spikes, 0.0,  t, objects[:i_read]) for t ∈ [150.0, 300.0, 450.0, 600.0, 750.0, 900.0, 1050.0, 1200.0, 1350.0, 1500.0]];
 ])
 
 logger=simulate!(net, input)
 
 n_storage   = filter(x->(x.object==:n_storage), logger.data)
+n_storage_spikes = filter(x->(x.object==:n_storage && x.event == :spikes), logger.data)
 n_readout   = filter(x->(x.object==:n_readout), logger.data)
 syn1   = filter(x->(x.object==:syn1), logger.data)
 syn2   = filter(x->(x.object==:syn2), logger.data)
-syn3   = filter(x->(x.object==:syn5), logger.data)
+syn5   = filter(x->(x.object==:syn5), logger.data)
 
 
 steps!(ax2, [0;syn1.t;1000], 3 .+ 0.45 .* [0;Int.(syn1.state);0] , fill=RGBAf0(1,0,0,0.5))
 steps!(ax2, [0;syn2.t;1000], 4 .+ 0.45 .* [0;Int.(syn2.state);0] , fill=RGBAf0(0,0,1,0.5))
-steps!(ax2, [0;syn3.t;1000], 1 .+ 0.45 .* [0;Int.(syn3.state);0] , fill=RGBAf0(1,0,1,0.5))
-steps!(ax2, [0;n_storage.t;1000], 2 .+ 0.45 .* [0;Int.(n_storage.state);0] , fill=RGBAf0(0,1,0,0.5))
+steps!(ax2, [0;syn5.t;1000], 1 .+ 0.45 .* [0;Int.(syn5.state);0] , fill=RGBAf0(1,0,1,0.5))
+linesegments!(ax2, repeat(n_storage_spikes.t,inner=2), repeat([2,2.9], outer=length(n_storage_spikes.t)))
+#steps!(ax2, [0;n_storage.t;1000], 2 .+ 0.45 .* [0;Int.(n_storage.state);0] )#, fill=RGBAf0(0,1,0,0.5))
 steps!(ax2, [0;n_readout.t;1000], 0.45 .* [0;Int.(n_readout.state);0] , fill=RGBAf0(0,1,1,0.5))
 
 ################################################################################
